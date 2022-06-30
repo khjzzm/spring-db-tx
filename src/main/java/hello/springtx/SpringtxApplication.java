82fe9f14b3cb79fd54aae8c2e515098d5223470a
kimhyunjin <khjzzm@gmail.com>
2022-06-30 09:28:37 +0900

    트랜잭션 적용 확인, 패키지 이름 변경

--- src/main/java/hello/springtx/SpringtxApplication.java
+++ src/main/java/hello/springtx/SpringtxApplication.java
@@ -1,4 +1,4 @@
-package heelo.springtx;
+package hello.springtx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/hello/springtx/SpringtxApplicationTests.java
+++ src/test/java/hello/springtx/SpringtxApplicationTests.java
@@ -1,4 +1,4 @@
-package heelo.springtx;
+package hello.springtx;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
