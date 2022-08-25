0882149398f94db7bcb3c89ac60f24b53055a768
kimhyunjin <khjzzm@gmail.com>
2022-08-25 15:22:50 +0900

    트랜잭션 전파 활용7 - 복구 REQUIRES_NEW

--- src/main/java/hello/springtx/propagation/LogRepository.java
+++ src/main/java/hello/springtx/propagation/LogRepository.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class LogRepository {
     private final EntityManager em;
 
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void save(Log logMessage) {
         log.info("log 저장");
         em.persist(logMessage);
